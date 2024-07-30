f8d8061d71c653214147811884e32aef0a10be1f
QiuLin-hao <210554114@qq.com>
2024-07-30 22:05:32 +0800

    添加注释，添加一个获取用户信息的api

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -3,11 +3,24 @@ import request from '@/utils/request'
 const api_name: string = '/api'
 
 export default {
+  /**
+   * 判断账号密码是否正确
+   * @param data
+   */
   login(data: any) {
     return request({
       url: `${api_name}/login`,
       method: 'post',
       data
+    })
+  },
+  /**
+   * 获取用户信息
+   */
+  getUserInfo() {
+    return request({
+      url: `${api_name}/user`,
+      method: 'get'
     })
   }
 }
